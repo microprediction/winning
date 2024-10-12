fd6029a7a369d19c031658f73609a43ddb2effc6
pcotton <pcotton@intechinvestments.com>
2024-10-12 18:43:17 -0400

    blah

--- examples_m6/infer_five_from_one.py
+++ examples_m6/infer_five_from_one.py
@@ -1,9 +1,11 @@
 from winning.lattice import skew_normal_density
-
+import math
+import matplotlib
 
 if __name__=='__main__':
     from winning.lattice_plot import densitiesPlot
     a = 1.0  # Skew parameter
-    densities = [skew_normal_density(L=100, unit=0.1, scale=1.0, loc=0, a=a) for _ in range(5)]
-    from winning.lattice_calibration import state_price_implied_ability
-    abilities = state_price_implied_ability(densities)
+    densities = [skew_normal_density(L=100, unit=0.1, scale=1.0, loc=1.25*l-1.25/2, a=-1) for l in range(5)]
+    densitiesPlot(densities, unit=0.1)
+    matplotlib.pyplot.figure(num=None, figsize=None, dpi=3000, facecolor=None, edgecolor=None, frameon=True)
+
